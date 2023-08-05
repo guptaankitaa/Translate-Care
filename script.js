c5143cb0576b0fe7f62c9007868c18ba9d8e1d2f
Ankita Gupta <112807231+Ankita-k-codes@users.noreply.github.com>
2023-08-05 10:01:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+
+
+const loginPopup = document.querySelector(".login-popup");
+const close = document.querySelector(".close");
+
+
+window.addEventListener("load",function(){
+
+ showPopup();
+ // setTimeout(function(){
+ //   loginPopup.classList.add("show");
+ // },5000)
+
+})
+
+function showPopup(){
+      const timeLimit = 3 // seconds;
+      let i=0;
+      const timer = setInterval(function(){
+       i++;
+       if(i == timeLimit){
+        clearInterval(timer);
+        loginPopup.classList.add("show");
+       } 
+       console.log(i)
+      },1000);
+}
+
+
+
+
+
+
+close.addEventListener("click",function(){
+  loginPopup.classList.remove("show");
+})
+
+
+
+  const loginPopup = document.querySelector(".login-popup");
+  const close = document.querySelector(".close");
+
+
+  window.addEventListener("load",function(){
+ 
+   showPopup();
+   // setTimeout(function(){
+   //   loginPopup.classList.add("show");
+   // },5000)
+
+  })
+
+  function showPopup(){
+        const timeLimit = 3 // seconds;
+        let i=0;
+        const timer = setInterval(function(){
+         i++;
+         if(i == timeLimit){
+          clearInterval(timer);
+          loginPopup.classList.add("show");
+         } 
+         console.log(i)
+        },1000);
+  }
+
+  
+
+  
+
+
+  close.addEventListener("click",function(){
+    loginPopup.classList.remove("show");
+  })
+
